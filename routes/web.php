0fab0832d4dd439a93189441aba009ad9747666d
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-06-08 11:01:31 +0900

    ルーティングの一括化

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,26 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [PostController::class, 'index']);
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 
-Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+// Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::resource('posts', PostController::class);
 
 Auth::routes();
 
