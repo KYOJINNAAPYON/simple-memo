503fca9be6e81b1240f73b1bcb7df2a51cec531c
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-06-08 10:01:31 +0900

    詳細を表示させるshow

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/posts/create', [PostController::class, 'create'])->name('posts.crea
 
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
