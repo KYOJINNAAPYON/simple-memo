1227d00f986113d4222a040c4e9bee60de9407b8
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-06-11 20:54:29 +0900

    ヘッダーのナビゲーションメニュー色変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 <header>
 
-  <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+  <nav class="navbar navbar-expand-md navbar-light bg-primary fixed-top">
      <div class="container">
-         <a class="navbar-brand" href="{{ url('/') }}">
+         <a class="navbar-brand text-light" href="{{ url('/') }}">
              {{ config('app.name', 'Laravel') }}
          </a>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -21,18 +21,18 @@
                  @guest
                      @if (Route::has('login'))
                          <li class="nav-item">
-                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                             <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
                          </li>
                      @endif
  
                      @if (Route::has('register'))
                          <li class="nav-item">
-                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                             <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
                          </li>
                      @endif
                  @else
                      <li class="nav-item dropdown">
-                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                         <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                              {{ Auth::user()->name }}
                          </a>
  
